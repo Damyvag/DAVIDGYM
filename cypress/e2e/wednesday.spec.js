560b021e86d59fa97bcfe984932867640374e23d
Damyvag <damyvag@hotmail.com>
2025-12-02 18:58:58 +0100

    initial commit

---
+++ cypress/e2e/wednesday.spec.js
@@ -0,0 +1,34 @@
+describe('Wednesday Page', () => {
+  it('should schedule a wednesday class', () => {
+    cy.visit('/');
+    //Add name
+    cy.get('input[name="myusername"]').click();
+    cy.get('input[name="myusername"]').should('have.focus');
+    cy.get('input[name="myusername"]').type('Darnedo');
+
+    //Add pwd
+    cy.get('input[name="mypassword"]').click();
+    cy.get('input[name="mypassword"]').should('have.focus');
+    cy.get('input[name="mypassword"]').type('abc123');
+
+    //Click Enter
+    cy.get('input[class="btn-block btn btn-primary btn-justified"]').click();
+
+    //Go to Reservas
+    cy.get(':nth-child(4) > .btn').click();
+
+    //Go to Activitats dirigides
+    cy.get(
+      '[href="/cat/poliwin/groupclasses/2025-12-02/d41d8cd98f00b204e9800998ecf8427e"]'
+    ).click();
+
+    //Select Friday Bodypump 17:15
+    cy.get('[id*="1715_72"]').click();
+
+    //Accept popup
+    cy.get('#Reservar').click();
+
+    //Accept popup
+    cy.get('#Tancar').click();
+  });
+});
